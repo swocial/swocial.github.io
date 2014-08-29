b105ef5de91b7335d9bb6a6644455249471c2bde
Arne Hassel <arne.hassel@gmail.com>
2014-08-29 23:25:05 +0200

    Doing a bit of cleaning up after epub generating

--- grunt/aliases.js
+++ grunt/aliases.js
@@ -1,6 +1,6 @@
 module.exports = {
   'publish': ['github'],
   'github': ['clean:githubPre', 'markdown:github', 'rename:github', 'clean:githubPost', 'sass:github'],
-  'epub': ['concat:epub', 'markdown:epub', 'shell:generateEpub'],
+  'epub': ['clean:epubPre', 'concat:epub', 'markdown:epub', 'shell:generateEpub', 'clean:epubPost'],
   'server': ['http-server:github']
 };
