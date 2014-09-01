90f80d8a0c22633b069281b24638721d773406c5
Arne Hassel <arne.hassel@gmail.com>
2014-09-01 22:54:07 +0200

    Made watch simpler, and edited README a bit

--- grunt/watch.js
+++ grunt/watch.js
@@ -1,10 +1,4 @@
 module.exports = {
-	publish: {
-		files: ['book/**/*.md'],
-		tasks: ['publish']
-	},
-  gh: {
-    files: ['book/**/*.md', 'templates/*.html', 'sass/github.scss'],
-    tasks: ['github']
-  }
+  files: ['book/**/*.md', 'templates/*.html', 'sass/github.scss'],
+  tasks: ['default']
 };
