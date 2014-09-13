7ffd91e8768070923758f2194299be6980ee813c
Arne Hassel <arne.hassel@gmail.com>
2014-09-13 14:58:12 +0200

    Implemented subset of acronym
    Should be enough for my use

--- test/acronym.test.js
+++ test/acronym.test.js
@@ -1,5 +1,6 @@
 var buster = require('buster')
-		assert = buster.referee.assert;
+		assert = buster.referee.assert,
+		refute = buster.referee.refute;
 buster.spec.expose();
 
 describe('acronym', function () {
@@ -22,12 +23,12 @@ describe('acronym', function () {
 	});
 
 	describe('acrodef', function () {
-		it('should not contain any acronyms by default', function () {
+		beforeEach(function () {
 			assert.equals(acronym.acronyms, {});
+			parser.parse('{{acrodef,etacar,$\\eta$ Car,Eta Carinae}}');
 		});
 
 		it('can be used to add full definition for acronyms', function () {
-			parser.parse('{{acrodef,etacar,$\\eta$ Car,Eta Carinae}}');
 			assert.equals(acronym.acronyms.etacar, {
 				acronym: '$\\eta$ Car',
 				full: 'Eta Carinae',
@@ -38,7 +39,40 @@ describe('acronym', function () {
 		it('supports outputting as LaTeX', function () {
 			process.env.FORMAT = 'latex';
 			var parsedText = parser.parse('{{acrodef,etacar,$\\eta$ Car,Eta Carinae}}');
-			assert.equals(parsedText, '\\acrodef{etacar}[$\\eta$ Car]{Eta Carinae}');
+			assert.equals(parsedText, '\\acrodef{etacar}{@{$\\eta$ Car}@}{Eta Carinae}');
+		});
+	});
+
+	describe('acronym', function () {
+		beforeEach(function () {
+			parser.parse('{{acronym,etacar,Eta Carinae}}');
+		});
+
+		it('can be used to add definitions for acronyms', function () {
+			assert.equals(acronym.acronyms.etacar, {
+				acronym: 'etacar',
+				full: 'Eta Carinae',
+				used: false
+			});
+		});
+
+		it('supports outputting as LaTeX', function () {
+			process.env.FORMAT = 'latex';
+			var parsedText = parser.parse('{{acronym,etacar,Eta Carinae}}');
+			assert.equals(parsedText, '\\acrodef{etacar}{@{etacar}@}{Eta Carinae}');
+		});
+	});
+
+	describe('acresetall', function () {
+		beforeEach(function () {
+			acronym.addAcronym('foo', 'f', 'Foo');
+			acronym.useAcronym('foo');
+			assert(acronym.acronyms.foo.used);
+			parser.parse('{{acresetall}}');
+		});
+
+		it('should reset uses of acronyms', function () {
+			refute(acronym.acronyms.foo.used);
 		});
 	});
 
@@ -62,6 +96,98 @@ describe('acronym', function () {
 			process.env.FORMAT = 'html';
 			var parsedText = parser.parse('{{ac,foo}} test {{ac,foo}}');
 			assert.equals(parsedText, 'Foo (f) test <abbr title="Foo">f</abbr>');
+		});
+	});
+
+	describe('acf', function () {
+		beforeEach(function () {
+			acronym.addAcronym('dsn', 'DSN', 'Distributed Social Network');
+		});
+
+		it('should set status to used once parsed', function () {
+			parser.parse('{{acf,dsn}}');
+			assert.equals(acronym.acronyms.dsn.used, true);
+		});
+
+		it('supports outputting as LaTeX', function () {
+			process.env.FORMAT = 'latex';
+			var parsedText = parser.parse('{{acf,dsn}} test {{acf,dsn}}');
+			assert.equals(parsedText, '\\acf{dsn} test \\acf{dsn}');
+		});
+
+		it('supports outputting as HTML', function () {
+			process.env.FORMAT = 'html';
+			var parsedText = parser.parse('{{acf,dsn}} test {{acf,dsn}}');
+			assert.equals(parsedText, 'Distributed Social Network (DSN) test Distributed Social Network (DSN)');
+		});
+	});
+
+	describe('acs', function () {
+		beforeEach(function () {
+			acronym.addAcronym('dsn', 'DSN', 'Distributed Social Network');
+		});
+
+		it('should set status to used once parsed', function () {
+			parser.parse('{{acs,dsn}}');
+			assert.equals(acronym.acronyms.dsn.used, true);
+		});
+
+		it('supports outputting as LaTeX', function () {
+			process.env.FORMAT = 'latex';
+			var parsedText = parser.parse('{{acs,dsn}} test {{acs,dsn}}');
+			assert.equals(parsedText, '\\acs{dsn} test \\acs{dsn}');
+		});
+
+		it('supports outputting as HTML', function () {
+			process.env.FORMAT = 'html';
+			var parsedText = parser.parse('{{acs,dsn}} test {{acs,dsn}}');
+			assert.equals(parsedText, '<abbr title="Distributed Social Network">DSN</abbr> test <abbr title="Distributed Social Network">DSN</abbr>');
+		});
+	});
+
+	describe('acl', function () {
+		beforeEach(function () {
+			acronym.addAcronym('dsn', 'DSN', 'Distributed Social Network');
+		});
+
+		it('should set status to used once parsed', function () {
+			parser.parse('{{acl,dsn}}');
+			assert.equals(acronym.acronyms.dsn.used, true);
+		});
+
+		it('supports outputting as LaTeX', function () {
+			process.env.FORMAT = 'latex';
+			var parsedText = parser.parse('{{acl,dsn}} test {{acl,dsn}}');
+			assert.equals(parsedText, '\\acl{dsn} test \\acl{dsn}');
+		});
+
+		it('supports outputting as HTML', function () {
+			process.env.FORMAT = 'html';
+			var parsedText = parser.parse('{{acl,dsn}} test {{acl,dsn}}');
+			assert.equals(parsedText, 'Distributed Social Network test Distributed Social Network');
+		});
+	});
+
+	describe('acp', function () {
+		beforeEach(function () {
+			acronym.addAcronym('dsn', 'DSN', 'Distributed Social Network');
+		});
+
+		it('should set status to used once parsed', function () {
+			parser.parse('{{acp,dsn}}');
+			assert.equals(acronym.acronyms.dsn.used, true);
+		});
+
+		it('supports outputting as LaTeX', function () {
+			process.env.FORMAT = 'latex';
+			var parsedText = parser.parse('{{acp,dsn}} test {{acp,dsn}}');
+			assert.equals(parsedText, '\\acp{dsn} test \\acp{dsn}');
+		});
+
+		it('supports outputting as HTML', function () {
+			process.env.FORMAT = 'html';
+			var parsedText = parser.parse('{{acp,dsn}} test {{acp,dsn}}');
+			assert.equals(parsedText, 'Distributed Social Networks (DSNs) test <abbr title="Distributed Social Networks">DSNs</abbr>');
 		});
 	});
 });
--- tools/acronym.js
+++ tools/acronym.js
@@ -1,3 +1,6 @@
+// implemented based on http://staff.science.uva.nl/~polko/HOWTO/LATEX/acronym.html
+// am looking at http://mirrors.ctan.org/macros/latex/contrib/acronym/acronym.pdf, but haven't use for all functions
+
 var util = require('util');
 
 module.exports = function (parser) {
@@ -23,6 +26,16 @@ module.exports = function (parser) {
 		}
 		return '';
 	};
+	Acronym.prototype.outputAcronym = function (label) {
+		var ac = this.acronyms[label];
+		switch(process.env.FORMAT) {
+			case 'html':
+				return util.format('<abbr title="%s">%s</abbr>', ac.full, ac.acronym)
+			case 'latex':
+				return util.format('\\acs{%s}', label);
+		}
+		return '';
+	};
 	Acronym.prototype.outputDefinition = function (label) {
 		var ac = this.acronyms[label];
 		switch(process.env.FORMAT) {
@@ -30,6 +43,43 @@ module.exports = function (parser) {
 				return util.format('\\acrodef{%s}{@{%s}@}{%s}', label, ac.acronym, ac.full);
 		}
 		return '';
+	};
+	Acronym.prototype.outputFull = function (label) {
+		var ac = this.acronyms[label];
+		switch(process.env.FORMAT) {
+			case 'html':
+				return util.format('%s (%s)', ac.full, ac.acronym)
+			case 'latex':
+				return util.format('\\acf{%s}', label);
+		}
+		return '';
+	};
+	Acronym.prototype.outputLong = function (label) {
+		var ac = this.acronyms[label];
+		switch(process.env.FORMAT) {
+			case 'html':
+				return ac.full;
+			case 'latex':
+				return util.format('\\acl{%s}', label);
+		}
+		return '';
+	};
+	Acronym.prototype.outputPlural = function (label) {
+		var ac = this.acronyms[label];
+		switch(process.env.FORMAT) {
+			case 'html':
+				return ac.used
+					? util.format('<abbr title="%ss">%ss</abbr>', ac.full, ac.acronym)
+					: util.format('%ss (%ss)', ac.full, ac.acronym)
+			case 'latex':
+				return util.format('\\acp{%s}', label);
+		}
+		return '';
+	};
+	Acronym.prototype.resetAll = function () {
+		for (var key in this.acronyms) {
+			this.acronyms[key].used = false;
+		}
 	};
 	Acronym.prototype.useAcronym = function (label) {
 		this.acronyms[label].used = true;
@@ -40,28 +90,37 @@ module.exports = function (parser) {
 			instance.addAcronym(label, acronym, full);
 			return instance.outputDefinition(label);
 		},
-		'acronym': function () {
-
+		'acronym': function (label, full) {
+			instance.addAcronym(label, label, full);
+			return instance.outputDefinition(label);
 		},
 		'acresetall': function () {
-
+			instance.resetAll();
 		},
 		'ac': function (label) {
 			var ac = instance.output(label);
 			instance.useAcronym(label);
 			return ac;
 		},
-		'acf': function () {
-
+		'acf': function (label) {
+			var acf = instance.outputFull(label);
+			instance.useAcronym(label);
+			return acf;
 		},
-		'acs': function () {
-
+		'acs': function (label) {
+			var acs = instance.outputAcronym(label);
+			instance.useAcronym(label);
+			return acs;
 		},
-		'acl': function () {
-
+		'acl': function (label) {
+			var acl = instance.outputLong(label);
+			instance.useAcronym(label);
+			return acl;
 		},
-		'acp': function () {
-
+		'acp': function (label) {
+			var acp = instance.outputPlural(label);
+			instance.useAcronym(label);
+			return acp;
 		}
 	});
 	return instance;
